351782c72d0d70e25f0bbf0e2d3d7bf627698274
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-03-29 14:47:24 -0300

    Create day07.js
    Add day07.js file

---
+++ Day07/day07.js
@@ -0,0 +1,28 @@
+// projeto calculadora
+
+// inserir dados
+function insert(num) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num
+}
+
+// função limpar calculadora
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+//função apagar um numero da direita para a esquerda
+function back() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+
+// função calcular
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+        document.getElementById('resultado').innerHTML = "nada...";
+    }
+}
